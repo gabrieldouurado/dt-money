ef4b2d795919b1ba5b47eb67da32e0ae4e363f7c
João Gabriel Dourado <joao.dourado96@gmail.com>
2023-03-01 11:01:05 -0300

    Update project for github

---
+++ README.md
@@ -0,0 +1,7 @@
+# DT-Money
+
+Projeto de uma interface para controlar gastos e lucros.
+
+Criado durante a especialização em React da Rocketseat.
+
+Módulo focado na crição de uma aplicação front-end web completa, com consumo de API e também técnicas de como performar aplicações com o React entendendo como funcionam os algoritmos internos da biblioteca e todo fluxo de renderização de componentes.
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,24 @@
+import * as Dialog from '@radix-ui/react-dialog'
+
+import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles'
+
+import logoImage from '../../assets/logo.svg'
+import { NewTransactionModal } from '../NewTransactionModal'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <HeaderContent>
+        <img src={logoImage} alt="" />
+
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova Transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <NewTransactionModal />
+        </Dialog.Root>
+      </HeaderContent>
+    </HeaderContainer>
+  )
+}
